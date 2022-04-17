f8f0887d8232dd639995434b32f1f0d8aa484669
Nikita Goncharenko <75169515+NGPetrovich@users.noreply.github.com>
2022-04-17 16:06:02 +0200

    added multiple languages news

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,25 @@
 const axios = require("axios");
+const countries = require("./countries.json");
 require("dotenv").config();
 
-module.exports = async function () {
+async function getNews(country) {
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=be&apiKey=${process.env.NEWS_SECRET_KEY}&pageSize=7`
+      `https://newsapi.org/v2/top-headlines?country=${country}&category=sport&apiKey=${process.env.NEWS_SECRET_KEY}&pageSize=3`
     );
-    return response.data;
+    return {
+      country: country,
+      articles: response.data.articles,
+    };
   } catch (error) {
     console.error(error);
   }
+}
+
+module.exports = async function () {
+  const newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then((newsObjects) => {
+    console.log("newsObjects:", newsObjects);
+    return [].concat.apply([], newsObjects);
+  });
 };
