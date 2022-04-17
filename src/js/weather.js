85f1a2dd1eccf8643f5baf46b51da3c443bb67a2
Nikita Goncharenko <75169515+NGPetrovich@users.noreply.github.com>
2022-04-17 18:17:13 +0200

    finish the project

---
+++ src/js/weather.js
@@ -0,0 +1,24 @@
+function success(pos) {
+  const url = `/.netlify/functions/weatherapi?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&units=metric`;
+
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      document.querySelector("#city").textContent = data.name;
+      document.querySelector("#temp").textContent = data.main.temp + "'C";
+      document.querySelector("#main").textContent = data.weather[0].main;
+      document.querySelector("#desc").textContent = data.weather[0].description;
+
+      document.querySelector("#weather").classList.remove("hidden");
+    });
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success, error);
+}
